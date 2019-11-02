f99d27e87665da5273d7e661d1ca4a4aa9dbc9fc
DanIel Brussee <dan@Dans-Macbook.nc.rr.com>
2019-11-02 09:27:54 -0400

    Update mainly dealing with dialogs

--- js/B.js
+++ js/B.js
@@ -133,12 +133,40 @@ var B;
         function Dialog(id) {
             this.id = "";
             this.domObj = null;
+            this.container = null;
             this.form = null;
             if (B.Dialog.dialogs[id] != undefined) {
                 return B.Dialog.get(id);
             }
             this.id = id;
-            this.domObj = document.getElementById(id);
+            var el = document.getElementById(id);
+            // Create a container for the dialog
+            this.domObj = document.createElement("div");
+            this.domObj.className = "BDialog";
+            this.domObj.style.cssText = "display:none; position:absolute;";
+            this.domObj.style.height = el.style.height + 25; // Room for a button box
+            this.domObj.style.width = el.style.width;
+            el.insertAdjacentElement("beforebegin", this.domObj);
+            el.className = "";
+            // Move the form into the container
+            this.domObj.appendChild(el);
+            // Add a button box for the bottom of the container
+            var bbox = document.createElement("div");
+            bbox.style.cssText = "border-top: 1px solid silver; padding-top: .4rem; text-align: right;";
+            this.domObj.appendChild(bbox);
+            var btns = el.getElementsByClassName("BDialogButton");
+            if (btns.length == 0) {
+                var btn = document.createElement("button");
+                btn.innerHTML = "Close";
+                btn.onclick = popDialog;
+                bbox.appendChild(btn);
+            }
+            else {
+                while (btns.length > 0) {
+                    var btn = btns.item(0);
+                    bbox.appendChild(btn);
+                }
+            }
             // TODO make the div pretty, etc
             B.Dialog.dialogs[id] = this;
             B.Dialog.dialogCount++;
@@ -146,21 +174,19 @@ var B;
                 B.Dialog.overlay = document.createElement("div");
                 document.body.appendChild(B.Dialog.overlay);
                 B.Dialog.overlay.style.cssText =
-                    "postion: fixed; " +
+                    "position: absolute; " +
                         "display: none; " + /* Hidden by default */
-                        "width: 100vw; " + /* Full width (cover the whole page) */
-                        "height: 100vh; " + /* Full height (cover the whole page) */
-                        "top: 0; " +
-                        "left: 0; " +
-                        //"right: 0; " +
-                        //"bottom: 0; " +
-                        "background-color: rgba(0,0,0,0.3); "; /* Black background with opacity */
-                //"z-index: 2; "; /* Specify a stack order in case you're using a different order for other elements */
+                        "width: 100%; height:100%; " + /* Full width (cover the whole page) */
+                        "top: 0; left: 0; right: 0; bottom: 0; " +
+                        "margin:0; padding:0; border: 0; " +
+                        "background: rgba(0,0,0,0.3); "; /* Black background with opacity */
+                "z-index: 1; "; /* Specify a stack order in case you're using a different order for other elements */
             }
             this.form = new B.Form(id);
         }
         Dialog.prototype.isOpen = function () { return (this.domObj.style.display == "inline-block"); };
         Dialog.prototype.open = function (center) {
+            if (center === void 0) { center = true; }
             if (this.isOpen())
                 return;
             this.domObj.style.display = "inline-block";
@@ -184,8 +210,15 @@ var B;
             this.domObj.style.display = "none";
             for (var i = 0; i < B.Dialog.dialogStack.length; i++) {
                 if (B.Dialog.dialogStack[i] == this.id) {
-                    B.Dialog.dialogStack = B.Dialog.dialogStack.splice(i, 1);
+                    B.Dialog.dialogStack.splice(i, 1);
                 }
+            }
+            if (B.Dialog.dialogStack.length == 0) {
+                B.Dialog.overlay.style.display = "none";
+            }
+            else {
+                var z = ((B.Dialog.dialogStack.length - 1) * 2) + 10;
+                B.Dialog.overlay.style.zIndex = z;
             }
         };
         Dialog.get = function (id) {
--- js/ts/BDialog.ts
+++ js/ts/BDialog.ts
@@ -7,13 +7,42 @@ namespace B {
         public static dialogCount:number = 0;
         public static dialogStack = [];
         private domObj:HTMLElement = null;
+        private container = null;
         public form:Form = null;
         constructor(id:string) {
             if (B.Dialog.dialogs[id] != undefined) {
                 return B.Dialog.get(id);
             }
             this.id = id;
-            this.domObj = document.getElementById(id);
+            var el = document.getElementById(id);
+            // Create a container for the dialog
+            this.domObj = document.createElement("div");
+            this.domObj.className = "BDialog";
+            this.domObj.style.cssText = "display:none; position:absolute;"
+            this.domObj.style.height = el.style.height + 25; // Room for a button box
+            this.domObj.style.width = el.style.width;
+            el.insertAdjacentElement("beforebegin", this.domObj);
+            el.className = "";
+            // Move the form into the container
+            this.domObj.appendChild(el);
+
+            // Add a button box for the bottom of the container
+            let bbox = document.createElement("div");
+            bbox.style.cssText = "border-top: 1px solid silver; padding-top: .4rem; text-align: right;"
+            this.domObj.appendChild(bbox);
+            let btns = el.getElementsByClassName("BDialogButton");
+            if (btns.length == 0) {
+                let btn = document.createElement("button");
+                btn.innerHTML = "Close";
+                btn.onclick = popDialog;
+                bbox.appendChild(btn);    
+            } else {
+                while (btns.length > 0) {
+                    let btn = btns.item(0);
+                    bbox.appendChild(btn);
+                }    
+            }
+
             // TODO make the div pretty, etc
             B.Dialog.dialogs[id] = this;
             B.Dialog.dialogCount++;
@@ -21,22 +50,18 @@ namespace B {
                 B.Dialog.overlay = document.createElement("div");
                 document.body.appendChild(B.Dialog.overlay);
                 B.Dialog.overlay.style.cssText = 
-                    "postion: fixed; " +
+                    "position: absolute; " +
                     "display: none; " +/* Hidden by default */
-                    "width: 100vw; " +/* Full width (cover the whole page) */
-                    "height: 100vh; " + /* Full height (cover the whole page) */
-                    "top: 0; " +
-                    "left: 0; " +
-                    //"right: 0; " +
-                    //"bottom: 0; " +
-                    "background-color: rgba(0,0,0,0.3); "; /* Black background with opacity */
-                    //"z-index: 2; "; /* Specify a stack order in case you're using a different order for other elements */
-
+                    "width: 100%; height:100%; " +/* Full width (cover the whole page) */
+                    "top: 0; left: 0; right: 0; bottom: 0; " +
+                    "margin:0; padding:0; border: 0; " +
+                    "background: rgba(0,0,0,0.3); "; /* Black background with opacity */
+                    "z-index: 1; "; /* Specify a stack order in case you're using a different order for other elements */
             }
             this.form = new B.Form(id);
         }   
         isOpen() { return (this.domObj.style.display == "inline-block") }
-        open(center?:boolean) {
+        open(center:boolean=true) {
             if (this.isOpen()) return;
             this.domObj.style.display = "inline-block";
             let z = (B.Dialog.dialogStack.length * 2) + 10;
@@ -57,8 +82,14 @@ namespace B {
             this.domObj.style.display = "none";
             for (let i = 0; i < B.Dialog.dialogStack.length; i++) {
                 if (B.Dialog.dialogStack[i] == this.id) {
-                    B.Dialog.dialogStack = B.Dialog.dialogStack.splice(i, 1);
+                    B.Dialog.dialogStack.splice(i, 1);
                 }
+            }
+            if (B.Dialog.dialogStack.length == 0) {
+                B.Dialog.overlay.style.display = "none";
+            } else {
+                let z = ((B.Dialog.dialogStack.length-1) * 2) + 10;
+                B.Dialog.overlay.style.zIndex = z;
             }
         }
         static get(id:string):Dialog {
